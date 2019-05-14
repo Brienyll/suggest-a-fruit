ebde7226acade7af961583ac82ec6e2c912a77c4
Brienyll <Brienyll29@gmail.com>
2019-05-13 22:23:28 -0700

    fixed bugs

--- src/helpers.js
+++ src/helpers.js
@@ -3,7 +3,7 @@ function choice(items) {
   return items[index];
 }
 
-function remove(items, item) {
+function remove(item, items) {
   for (let i = 0; i < items.length; i++) {
     if (items[i] === item) {
       return [...items.slice(0, i), ...items.slice(i + 1)]
